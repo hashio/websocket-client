cd32a1eb341a7e9ab6ec7a05db3671db6ef2b43f
Takahiro Hashimoto <mlg28671@nifty.com>
2011-03-05 16:55:27 +0900

    cleanup code

---
+++ subscriber-swing/src/main/java/jp/a840/push/subscriber/swing/panel/set/hash/RateHashTablePane.java
@@ -0,0 +1,20 @@
+package jp.a840.push.subscriber.swing.panel.set.hash;
+
+import jp.a840.push.subscriber.swing.listener.Filter;
+import jp.a840.push.subscriber.swing.listener.ThroughFilter;
+import jp.a840.push.subscriber.swing.panel.AbstractInfosTablePane;
+import jp.a840.push.subscriber.swing.table.BestRateHashTableModel;
+import jp.a840.push.subscriber.swing.table.SwingClientDefaultHashTableModel;
+
+public class RateHashTablePane extends AbstractInfosTablePane {
+
+	private Filter filter;
+	
+	public RateHashTablePane() {
+		super();
+		BestRateHashTableModel model = new BestRateHashTableModel();
+		Filter filter = new ThroughFilter(model);
+		setFilter(filter);
+	}
+
+}
